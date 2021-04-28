391ae4dde3bc0d4edcf99815d372d1e08f86954f
Fawzaan <fawzi20112001@gmail.com>
2021-04-28 22:07:43 +0530

    Added script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Function that prevents browser from going back
+function preventBack() { window.history.forward(); }  
+setTimeout("preventBack()", 0);  
+window.onunload = function () { null };
+
+// Function to work on the "I'm Ready" button 
+function startQuiz() {
+    window.location.href = "something.html";
+}
