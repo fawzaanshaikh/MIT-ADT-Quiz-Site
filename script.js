e9e5dfa1a9b109e40a9a18ac6ea65ef0d45ef4dd
Fawzaan <fawzi20112001@gmail.com>
2021-04-29 11:29:51 +0530

    Added more questions and tested the flow

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+/* Global Variables */
+var question_count = 1;
+
 // Function that prevents browser from going back
 function preventBack() { window.history.forward(); }  
 setTimeout("preventBack()", 0);  
@@ -5,5 +8,8 @@ window.onunload = function () { null };
 
 // Function to work on the "I'm Ready" button 
 function startQuiz() {
-    window.location.href = "something.html";
+    window.location.href = "question" + question_count + ".html";
+    question_count++;
 }
+
+
